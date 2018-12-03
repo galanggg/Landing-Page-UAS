eb9f3686bf77b66a9f2dbb3c713b56bf4cea48eb
galanggg <tugasgalang@gmail.com>
2018-12-03 21:08:05 +0800

    Done

---
+++ js/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FsYW5nZ2ciLCJhIjoiY2ptN25kZ3E4MGQwdzNrbGt2Z3oycDMwaCJ9.xesxKW7BUk26CUmZd7NOYg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [115.177579 , - 8.787636],
+    zoom: 15.10
+});
--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 let mobile = document.querySelector('.sidenav')
 M.Sidenav.init(mobile)
+
+let parallax = document.querySelector('.parallax-container')
+M.Parallax.init(parallax)
+
+var tampilan = document.querySelector('.tabs')
+M.Tabs.init(tampilan)
